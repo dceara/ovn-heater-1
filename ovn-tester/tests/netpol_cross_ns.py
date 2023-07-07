8357326ce13ffb10cf5eb75f608736999c99e569
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2023-07-07 18:23:13 +0200

    ovn-tester: extend netpol_cross_ns for ovn-ic
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>

--- ovn-tester/tests/netpol_cross_ns.py
+++ ovn-tester/tests/netpol_cross_ns.py
@@ -19,25 +19,16 @@ class NetpolCrossNs(ExtCmd):
 
     def run(self, clusters, global_cfg):
         all_ns = []
-
         with Context(
             clusters, 'netpol_cross_ns_startup', brief_report=True
         ) as ctx:
-            ovn = clusters[0]
-            ports = ovn.provision_ports(
-                self.config.pods_ns_ratio * self.config.n_ns
-            )
             for i in range(self.config.n_ns):
+                ovn = clusters[i % len(clusters)]
+                ports = ovn.provision_ports(self.config.pods_ns_ratio)
                 ns = Namespace(
                     [ovn], f'NS_netpol_cross_ns_startup_{i}', global_cfg
                 )
-                ns.add_ports(
-                    ports[
-                        i
-                        * self.config.pods_ns_ratio : (i + 1)
-                        * self.config.pods_ns_ratio
-                    ]
-                )
+                ns.add_ports(ports[0 : self.config.pods_ns_ratio])
                 if global_cfg.run_ipv4:
                     ns.default_deny(4)
                 if global_cfg.run_ipv6:
