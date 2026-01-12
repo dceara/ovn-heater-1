70e28d6eeb63683855a883ab53aa1195fc4277f2
Dumitru Ceara <dceara@redhat.com>
2026-01-12 16:42:22 +0100

    TODO: use almusil/ovn-fake-multinode/tree/unused
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- do.sh
+++ do.sh
@@ -118,8 +118,8 @@ ovn_repo="${OVN_REPO:-https://github.com/ovn-org/ovn.git}"
 ovn_branch="${OVN_BRANCH:-main}"
 
 # ovn-fake-multinode env vars
-ovn_fmn_repo="${OVN_FAKE_MULTINODE_REPO:-https://github.com/ovn-org/ovn-fake-multinode.git}"
-ovn_fmn_branch="${OVN_FAKE_MULTINODE_BRANCH:-main}"
+ovn_fmn_repo="${OVN_FAKE_MULTINODE_REPO:-https://github.com/almusil/ovn-fake-multinode.git}"
+ovn_fmn_branch="${OVN_FAKE_MULTINODE_BRANCH:-unused}"
 
 OS_BASE="${OS_BASE:-fedora}"
 OS_IMAGE_OVERRIDE="${OS_IMAGE_OVERRIDE}"
